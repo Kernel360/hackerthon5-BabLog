e87598ea8e74da5fbd40bf9693a78aab4bf9cdfd
cooper <pbg0205@naver.com>
2025-05-07 17:12:47 +0900

    feat: 리뷰 생성 API - DB 로직 작성

--- bablog-be/src/main/java/kr/bablog/bablogbe/reviews/service/errors/ReviewErrorType.java
+++ bablog-be/src/main/java/kr/bablog/bablogbe/reviews/service/errors/ReviewErrorType.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReviewErrorType {
 	USER_REVIEW_EXIST(ReviewErrorCode.ERROR_REVIEW01, "기존 사용자가 해당 리뷰를 작성하였습니다."),
+	REVIEW_CREATE_CONSTRAINT(ReviewErrorCode.ERROR_REVIEW02, "리뷰 생성에 있어 문제가 발생하였습니다.");
 
 	private final ReviewErrorCode errorCode;
 	private final String message;
