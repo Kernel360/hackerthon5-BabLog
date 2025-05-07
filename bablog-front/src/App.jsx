5781d295a4221ad088980ae9a46c4d44af03036d
myaeba <lim104321@naver.com>
2025-05-07 19:01:45 +0900

    feat : 회원가입 routes 작성

--- bablog-front/src/App.jsx
+++ bablog-front/src/App.jsx
@@ -1,3 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-up" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
