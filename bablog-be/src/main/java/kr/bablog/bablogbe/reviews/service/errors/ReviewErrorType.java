a0c0261319e106078cae406d9a960fd401e91323
cooper <pbg0205@naver.com>
2025-05-08 01:18:53 +0900

    feat: 리뷰 목록 API - 컨트롤러 작성
    - offset 0 미만일 경우 예외 반환
    - size 0 이하일 경우 예외 반환

--- bablog-be/src/main/java/kr/bablog/bablogbe/reviews/service/errors/ReviewErrorType.java
+++ bablog-be/src/main/java/kr/bablog/bablogbe/reviews/service/errors/ReviewErrorType.java
@@ -9,7 +9,9 @@ public enum ReviewErrorType {
 	USER_REVIEW_EXIST(ReviewErrorCode.ERROR_REVIEW01, "기존 사용자가 해당 리뷰를 작성하였습니다."),
 	REVIEW_CREATE_CONSTRAINT(ReviewErrorCode.ERROR_REVIEW02, "리뷰 생성에 있어 문제가 발생하였습니다."),
 	REVIEW_NOT_FOUND(ReviewErrorCode.ERROR_REVIEW03, "리뷰가 존재하지 않습니다"),
-	REVIEW_COMMENT_EMPTY(ReviewErrorCode.ERROR_REVIEW04, "리뷰 코멘트가 비어있습니다.");
+	REVIEW_COMMENT_EMPTY(ReviewErrorCode.ERROR_REVIEW04, "리뷰 코멘트가 비어있습니다."),
+	REVIEW_INVALID_OFFSET(ReviewErrorCode.ERROR_REVIEW05, "리뷰 오프셋는 0 이상이어야 합니다."),
+	REVIEW_INVALID_SIZE(ReviewErrorCode.ERROR_REVIEW06, "리뷰 사이즈는 0 보다 커야 합니다.");
 
 	private final ReviewErrorCode errorCode;
 	private final String message;
