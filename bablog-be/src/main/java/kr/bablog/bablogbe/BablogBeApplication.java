770c9875b72b01558f1d6372fb41bf4bb773b7d1
cooper <pbg0205@naver.com>
2025-05-07 11:08:39 +0900

    first commit

---
+++ bablog-be/src/main/java/kr/bablog/bablogbe/BablogBeApplication.java
@@ -0,0 +1,13 @@
+package kr.bablog.bablogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BablogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BablogBeApplication.class, args);
+	}
+
+}
