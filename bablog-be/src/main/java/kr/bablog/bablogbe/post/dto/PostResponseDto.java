ceb7566d0f6e563cd55d020547e56fe68ae1a8d2
안지현 <jhan-@naver.com>
2025-05-08 14:01:39 +0900

    refactor: 밥집 리스트 createdAt 필드 제거

--- bablog-be/src/main/java/kr/bablog/bablogbe/post/dto/PostResponseDto.java
+++ bablog-be/src/main/java/kr/bablog/bablogbe/post/dto/PostResponseDto.java
@@ -1,12 +1,10 @@
 package kr.bablog.bablogbe.post.dto;
 
 
-import java.time.LocalDateTime;
 
 public record PostResponseDto(
         Long id,
         String title,
         String address,
-        String imgUrl,
-        LocalDateTime createdAt) {
+        String imgUrl) {
 }
