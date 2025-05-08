aadaf7bd15175fdc0f671a77973f5d97cafda1f8
cooper <pbg0205@naver.com>
2025-05-08 12:15:22 +0900

    refactor: 리뷰 이메일 필드 더미 방식 변경
    - 회원 도메인 통합 이전에 userId 설정 진행
    - 이메일 필드 변경을 위한 커밋
    - 클라이언트 테스트를 위한 이메일 더미 데이터 추가

--- bablog-be/src/main/java/kr/bablog/bablogbe/reviews/controller/dto/response/ReviewLookUpWebResponse.java
+++ bablog-be/src/main/java/kr/bablog/bablogbe/reviews/controller/dto/response/ReviewLookUpWebResponse.java
@@ -2,12 +2,12 @@ package kr.bablog.bablogbe.reviews.controller.dto.response;
 
 import kr.bablog.bablogbe.reviews.service.dto.response.ReviewLookupResponse;
 
-public record ReviewLookUpWebResponse(Long reviewId, String comment, Long userId, boolean like) {
+public record ReviewLookUpWebResponse(Long reviewId, String comment, String email, boolean like) {
 	public static ReviewLookUpWebResponse from(ReviewLookupResponse reviewLookupResponse) {
 		return new ReviewLookUpWebResponse(
 			reviewLookupResponse.reviewId(),
 			reviewLookupResponse.comment(),
-			reviewLookupResponse.userId(),
+			"test@gmail.com", // TODO email 필드 주입 변경
 			reviewLookupResponse.like());
 	}
 }
