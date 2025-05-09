33da3f596b4ded215d985d03e6a1176e2f369828
cooper <pbg0205@naver.com>
2025-05-09 11:22:55 +0900

    fix: 포스트 상세 이미지 조회 안돼는 이슈 해결
    - 원인 : API 인터페이스 필드 설정 이슈로 인한 문제

--- bablog-front/src/components/DetailPage.jsx
+++ bablog-front/src/components/DetailPage.jsx
@@ -6,7 +6,7 @@ import '../styles/DetailPage.css';
 const DetailPage = ({ detailData }) => (
   <div className="detail-page">
     <TitleSection data={detailData} />
-    <ImageSection imageUrl={detailData.imageUrl} />
+    <ImageSection imageUrl={detailData.imgUrl} />
     <ReviewSection postId={detailData.postId} userId={detailData.userId} />
   </div>
 );
